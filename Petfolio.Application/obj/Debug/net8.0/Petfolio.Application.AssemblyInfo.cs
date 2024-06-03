4b0ffe4a75a31341e8c24977921e82ed41035260
Rafael Iuri Coppola <rafael.coppola@bmstecnologia.com.br>
2024-06-03 11:37:20 -0300

    finalizando projeto

--- Petfolio.Application/obj/Debug/net8.0/Petfolio.Application.AssemblyInfo.cs
+++ Petfolio.Application/obj/Debug/net8.0/Petfolio.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Petfolio.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3bec517414607afd12eefc045f86c750c66ae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3604d6fb720c87ac071f599da7c2d5f283d915bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Petfolio.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Petfolio.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
